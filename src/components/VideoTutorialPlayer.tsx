b22ea57eb60eb21f11dc24da69dc959d7d9f7503
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-29 14:18:35 +0000

    Crea interfaz de VideoPlayer

--- src/components/VideoTutorialPlayer.tsx
+++ src/components/VideoTutorialPlayer.tsx
@@ -1,3 +1,19 @@
 import React, { useRef, useState } from "react";
 import { Video } from "expo-av";
 import { StyleSheet, TouchableWithoutFeedback } from "react-native";
+
+const VideoPlayer: React.FC = () => {
+  return (
+    <TouchableWithoutFeedback onPress={togglePlay}>
+      <Video
+        ref={videoRef}
+        source={{ uri: "tu_url_del_video.mp4" }}
+        style={styles.video}
+        isLooping
+        shouldPlay
+      />
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default VideoPlayer;
