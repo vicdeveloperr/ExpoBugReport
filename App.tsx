4e6e170a70262f16b123bdfe97b41a851dec8aa6
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-30 17:38:54 +0000

    Revierte cambios en App.tsx
    Vuelve archivo al estado en que se encontraba en el commit c98dce306976489f25b810c1bdf4b40123f87ac4

--- App.tsx
+++ App.tsx
@@ -1,26 +1,6 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
-import { useEffect, useState } from "react";
-import { getStatusBarHeight } from "react-native-status-bar-height";
-import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  const [statusBarHeight, setStatusBarHeight] = useState<number>(0);
-
-  useEffect(() => {
-    const height = getStatusBarHeight();
-    setStatusBarHeight(height);
-  }, []);
-
-  return (
-    <View style={[styles.container, { marginTop: statusBarHeight }]}>
-      <StackNavigator />
-    </View>
-  );
+  return <StackNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
