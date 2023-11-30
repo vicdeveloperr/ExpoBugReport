9c1805da30764acbd1389ec3b3629b623db2af80
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-30 17:40:14 +0000

    Elimina utilidad getDeviceStatusBarHeight.ts

--- src/utils/getDeviceStatusBarHeight.ts
+++
@@ -1,7 +0,0 @@
-import { getStatusBarHeight } from "react-native-status-bar-height";
-
-function getDeviceStatusBarHeight() {
-  return getStatusBarHeight();
-}
-
-export default getDeviceStatusBarHeight;
