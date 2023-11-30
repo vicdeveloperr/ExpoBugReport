965a715e676b44db9666fdf9649483db66c3b2a9
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-30 17:15:45 +0000

    Crea utilidad getDeviceStatusBarHeight.ts
    Utiliza está función para obtener la altura de la barra de estado, cuando tu componente principal se renderice.

---
+++ src/utils/getDeviceStatusBarHeight.ts
@@ -0,0 +1,7 @@
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
+function getDeviceStatusBarHeight() {
+  return getStatusBarHeight();
+}
+
+export default getDeviceStatusBarHeight;
