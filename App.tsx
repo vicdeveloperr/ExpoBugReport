2999fee2c8b833b73b2248036fd909fa2811d92f
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-30 17:11:09 +0000

    Soluciona superposición de contenido y statusbar
    Utiliza la librería 'react-native-status-bar-height' para obtener el tamaño de la barra de estado y luego utilizarla como margin en el contenedor

--- App.tsx
+++ App.tsx
@@ -1,6 +1,26 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
+import { useEffect, useState } from "react";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
-  return <StackNavigator />;
+  const [statusBarHeight, setStatusBarHeight] = useState<number>(0);
+
+  useEffect(() => {
+    const height = getStatusBarHeight();
+    setStatusBarHeight(height);
+  }, []);
+
+  return (
+    <View style={[styles.container, { marginTop: statusBarHeight }]}>
+      <StackNavigator />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
