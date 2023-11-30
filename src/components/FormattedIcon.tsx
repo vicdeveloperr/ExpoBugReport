bef54510990cd6f4769fa8b13681629a999a6cbf
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-11-30 16:26:21 +0000

    Agrega tamaño 'small' a FormattedIcon.tsx

--- src/components/FormattedIcon.tsx
+++ src/components/FormattedIcon.tsx
@@ -4,10 +4,10 @@ import { primaryColor } from "../styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 interface FormattedIconProps {
   name: "controller-stop" | "controller-record" | "folder-video" | "camera";
-  size?: "big" | "medium";
+  size?: "big" | "medium" | "small";
 }
 const FormattedIcon: React.FC<FormattedIconProps> = ({ name, size }) => {
-  const iconSize = size === "medium" ? 57 : 74;
+  const iconSize = size === "medium" ? 57 : size === "small" ? 44 : 74;
   if (name === "camera") {
     return (
       <MaterialIcons
