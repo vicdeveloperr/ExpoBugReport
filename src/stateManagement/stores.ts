46faf9af1b49c954c6bf6ec27e7dbf735973899f
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-05 15:59:07 +0000

    Elimina 'useCountdownStore' de stores.ts

--- src/stateManagement/stores.ts
+++ src/stateManagement/stores.ts
@@ -1,15 +1,3 @@
 import { create } from "zustand";
 
-type countdownStore = {
-  countdown: number;
-  substractCountdown: () => void;
-};
 
-export const useCountdownStore = create<countdownStore>((set) => ({
-  countdown: 3,
-  substractCountdown: () =>
-    set(({ countdown }) => {
-      const newValue = countdown > 0 ? countdown - 1 : 3;
-      return { countdown: newValue };
-    }),
-}));
