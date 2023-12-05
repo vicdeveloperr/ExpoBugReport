eed7dbd86158b79b6561abeec44af42a840aeae9
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-05 16:00:03 +0000

    Define tipo countdownStore en stores.ts

--- src/stateManagement/stores.ts
+++ src/stateManagement/stores.ts
@@ -1,3 +1,8 @@
 import { create } from "zustand";
 
-
+type countdownStore = {
+  countdown: number;
+  startCountdown: (onFinish?: () => void) => void;
+  resetCountdown: () => void;
+  intervalId?: NodeJS.Timeout;
+};
