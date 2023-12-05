457dec62cb8cb4c7c9487e732302332b769ab50c
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-05 16:43:35 +0000

    Crea estructura de ProcesingVideoModal

--- src/components/ProcessingVideoModal.tsx
+++ src/components/ProcessingVideoModal.tsx
@@ -1,7 +1,15 @@
+import { Modal, View, Text, StyleSheet } from "react-native";
+
 const ProcessingVideoModal: React.FC = () => {
-    return (
-        
-    );
-}
+  return (
+    <Modal visible={true}>
+      <View>
+        <View>
+          <Text>Tu pequeño texto aquí</Text>
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 export default ProcessingVideoModal;
