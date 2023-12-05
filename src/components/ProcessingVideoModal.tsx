6bcb32419f60ecd171bb5eab7232b46d39a88be2
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-05 16:59:27 +0000

    Estiliza interfaz de ProcesingVideoModal

--- src/components/ProcessingVideoModal.tsx
+++ src/components/ProcessingVideoModal.tsx
@@ -1,15 +1,33 @@
 import { Modal, View, Text, StyleSheet } from "react-native";
+import { darkColor } from "../utils/colors";
 
 const ProcessingVideoModal: React.FC = () => {
   return (
     <Modal visible={true}>
-      <View>
-        <Text>Tu pequeño texto aquí</Text>
+      <View style={styles.container}>
+        <Text style={[styles.textBold, styles.text]}>
+          Estamos analizando su vídeo
+        </Text>
+        <Text style={[styles.text]}>Aguarde unos instantes...</Text>
       </View>
     </Modal>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: darkColor,
+    paddingVertical: 40,
+    paddingHorizontal: 10
+  },
+  textBold: {
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 19,
+    textAlign: "center",
+    color: "#fff",
+  },
+});
 
 export default ProcessingVideoModal;
