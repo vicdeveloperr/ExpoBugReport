19c8e186d0349529ff6f6193075dc14e47f9f2e5
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-06 13:43:23 +0000

    Estiliza ScreenDark

--- src/components/ScreenDark.tsx
+++ src/components/ScreenDark.tsx
@@ -1,6 +1,14 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 const ScreenDark: React.FC = () => {
   return <View style={styles.modal}></View>;
 };
 
+const styles = StyleSheet.create({
+  modal: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    display: "flex",
+    justifyContent: "center",
+  },
+});
 export default ScreenDark;
