e6f9ef599e8446b34d1a0417195b724d1b390dba
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-06 13:42:49 +0000

    Crea interfaz de ScreenDark

--- src/components/ScreenDark.tsx
+++ src/components/ScreenDark.tsx
@@ -1,3 +1,6 @@
-const ScreenDark: React.FC = () => {};
+import { View } from "react-native";
+const ScreenDark: React.FC = () => {
+  return <View style={styles.modal}></View>;
+};
 
 export default ScreenDark;
