bd5b88260cda713f8fd6a3144cfa582767b4429a
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-07 17:31:42 +0000

    Agrega 'alignItems: center' a ScreenDark

--- src/components/ScreenDark.tsx
+++ src/components/ScreenDark.tsx
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0,0,0,0.7)",
     display: "flex",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
 export default ScreenDark;
