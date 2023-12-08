45165ad7f6e5c81634c093fbb531ba6d53d8b446
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-08 15:44:12 +0000

    Utiliza 'StatusBar' en "auto"  en App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <StackNavigator />
+    </>
+  );
 }
