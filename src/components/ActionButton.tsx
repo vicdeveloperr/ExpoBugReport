47935376a79f161161873d58f4a21bd15a6f49f8
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-12 15:04:25 +0000

    Crea ActionButtonProps en ActionButton

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,6 +1,10 @@
-
-const ActionButton = () => {
-    // ..
+interface ActionButtonProps {
+  name: "continue" | "tryAgaint";
+  onPress: () => void;
 }
+
+const ActionButton: React.FC<ActionButtonProps> = ({ name, onPress }) => {
+  // ..
+};
 
 export default ActionButton;
