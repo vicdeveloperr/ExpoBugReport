5cabfe2351cf41aaf020321bf20d6276e647bf66
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-12 14:57:38 +0000

    Crea estructura de LoadVideoScreen

--- src/screens/LoadVideoScreen.tsx
+++ src/screens/LoadVideoScreen.tsx
@@ -1,5 +1,21 @@
+import { View } from "react-native";
+import { Video, ResizeMode } from "expo-av";
+import ScreenDark from "../components/ScreenDark";
+
 const LoadVideoScreen = () => {
-  // ..
+  return (
+    <View>
+      <Video
+        source={{
+          uri: "https://player.vimeo.com/external/467436330.sd.mp4?s=76304706368278640ac086aa2232c50327b2491e&profile_id=165&oauth2_token_id=57447761",
+        }}
+        resizeMode={ResizeMode.STRETCH}
+        isLooping
+        shouldPlay
+      />
+      <ScreenDark></ScreenDark>
+    </View>
+  );
 };
 
 export default LoadVideoScreen;
