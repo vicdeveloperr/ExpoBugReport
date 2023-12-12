c8e52db989905daf02422187a43560ec2d386ccd
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-12 15:30:00 +0000

    Crea estructura de la interfaz (ActionButton)

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,10 +1,16 @@
+import { TouchableOpacity, Text } from "react-native";
+
 interface ActionButtonProps {
   name: "continue" | "tryAgaint";
   onPress: () => void;
 }
 
 const ActionButton: React.FC<ActionButtonProps> = ({ name, onPress }) => {
-  // ..
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>{name === "continue" ? "Continuar" : "Volver a intentarlo"}</Text>
+    </TouchableOpacity>
+  );
 };
 
 export default ActionButton;
