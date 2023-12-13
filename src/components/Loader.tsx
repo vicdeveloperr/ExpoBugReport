3410038f5293a012b9d06fa54df678e43a9b4fcc
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-13 18:04:57 +0000

    Crea LoaderProps en Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import ProgressBar from "react-native-progress/Bar";
 
-const Loader = () => {};
+interface LoaderProps {
+  complete: boolean;
+}
+
+const Loader: React.FC<LoaderProps> = ({ complete }) => {};
 
 export default Loader;
