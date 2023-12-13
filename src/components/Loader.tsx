ceee74e4a49954a53bbf07e090a8c42190eed2d4
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-13 18:05:38 +0000

    Crea estado para Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,6 +5,8 @@ interface LoaderProps {
   complete: boolean;
 }
 
-const Loader: React.FC<LoaderProps> = ({ complete }) => {};
+const Loader: React.FC<LoaderProps> = ({ complete }) => {
+  const [progress, setProgress] = useState(0);
+};
 
 export default Loader;
