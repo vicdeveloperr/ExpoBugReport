0cb85e995ec6135ecfbe59d83f6d8728507cd3cb
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-13 18:07:35 +0000

    Crea useEffect actualizar de

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProgressBar from "react-native-progress/Bar";
 
 interface LoaderProps {
@@ -7,6 +7,16 @@ interface LoaderProps {
 
 const Loader: React.FC<LoaderProps> = ({ complete }) => {
   const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((progress + 0.05) % 1);
+    }, 300);
+
+    complete && setProgress(1);
+
+    return () => clearInterval(interval);
+  }, []);
 };
 
 export default Loader;
