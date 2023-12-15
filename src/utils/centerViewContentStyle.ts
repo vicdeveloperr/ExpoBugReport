2d57e98b21202e0ae7d266e9bb16c4bde43e568d
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-15 15:37:21 +0000

    Elimina centerViewContentStyle

--- src/utils/centerViewContentStyle.ts
+++
@@ -1,9 +0,0 @@
-import type { StyleProp, ViewStyle } from "react-native";
-
-const centerViewContentStyle: StyleProp<ViewStyle> = {
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-export default centerViewContentStyle;
