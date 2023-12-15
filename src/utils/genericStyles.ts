4621ebe4f3e54922be0d1e66b9ef328aa9cf7544
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-15 15:36:55 +0000

    Incluye centerViewContentStyle en genericStyles.ts

--- src/utils/genericStyles.ts
+++ src/utils/genericStyles.ts
@@ -1,4 +1,11 @@
 import { whiteColor } from "./colors";
+import type { StyleProp, ViewStyle } from "react-native";
+
+export const centerViewContentStyle: StyleProp<ViewStyle> = {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 export const paragraph = {
     color: whiteColor,
