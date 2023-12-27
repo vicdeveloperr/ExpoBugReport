9af2998087f80edcef5e920e3237236bdac8a34e
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2023-12-27 14:31:01 +0000

    Convierte en Modal ScreenDarkModal

--- src/components/ScreenDarkModal.tsx
+++ src/components/ScreenDarkModal.tsx
@@ -1,12 +1,22 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Modal } from "react-native";
 import type { StyleProp, ViewStyle } from "react-native";
 interface ScreenDarkProps {
   children: React.ReactNode;
   stylesView?: StyleProp<ViewStyle>;
 }
 
-const ScreenDarkModal: React.FC<ScreenDarkProps> = ({ children, stylesView }) => {
-  return <View style={[styles.modal, stylesView]}>{children}</View>;
+const ScreenDarkModal: React.FC<ScreenDarkProps> = ({
+  children,
+  stylesView,
+}) => {
+  return (
+    <Modal
+      visible={true}
+      animationType="fade"
+    >
+      <View style={[styles.modal, stylesView]}>{children}</View>
+    </Modal>
+  );
 };
 
 const styles = StyleSheet.create({
