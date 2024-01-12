b8bd7cce9c69b03cbcac2ab8d2bf2d75c6591420
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-12 13:34:15 +0000

    Instala y configura react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { StatusBar } from "expo-status-bar";
+import { PaperProvider } from "react-native-paper";
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <>
+    <PaperProvider>
       <StatusBar style="auto" />
       <StackNavigator />
-    </>
+    </PaperProvider>
   );
-}
+};
+
+export default App;
