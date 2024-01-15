ea15f9c8907c8bfdfbd8be12162fc88fdc0888cc
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-15 16:07:37 +0000

    Elimina testeo de btns del AlertDialog

--- src/__test__/AlertDialog.test.tsx
+++ src/__test__/AlertDialog.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/react-native/extend-expect";
-import { render, screen, fireEvent } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 import AlertDialog from "../components/AlertDialog";
 import { PaperProvider } from "react-native-paper";
 import { Text, Button } from "react-native";
@@ -36,19 +36,4 @@ describe("<AlertDialog />", () => {
     expect(AlertDialogFinishRecordButton).toBeDefined();
     expect(AlertDialogContinueRecordButton).toBeDefined();
   });
-
-//   it("cuando se clicka en el botón de continuar grabación del AlertDialogComponent, se oculta el cuadro de diálogo", () => {
-//     const AlertDialogComponent = screen.getByTestId("AlertDialog");
-//     const AlertDialogContinueRecordButton = screen.getByText("Continuar");
-
-//     fireEvent(AlertDialogContinueRecordButton, "press");
-//     expect(AlertDialogComponent).not.toBeDefined();
-//   });
-//   it("cuando se clicka en el botón de finalizar grabación del AlertDialogComponent, se oculta el cuadro de diálogo", () => {
-//     const AlertDialogComponent = screen.getByTestId("AlertDialog");
-//     const AlertDialogFinishRecordButton = screen.getByText("Finalizar");
-
-//     fireEvent(AlertDialogFinishRecordButton, "press");
-//     expect(AlertDialogComponent).not.toBeDefined();
-//   });
 });
