df449705811b99ea7558e6283b0dc5bbb6640b82
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-15 17:15:50 +0000

    Estiliza AlertDialog

--- src/components/AlertDialog.tsx
+++ src/components/AlertDialog.tsx
@@ -1,4 +1,7 @@
 import { Dialog, Portal } from "react-native-paper";
+import { darkColor, whiteColor } from "../utils/colors";
+import ScreenDarkModal from "./ScreenDarkModal";
+import { StyleSheet } from "react-native";
 
 interface AlertDialogProps {
   visible: boolean;
@@ -15,19 +18,24 @@ const AlertDialog: React.FC<AlertDialogProps> = ({
 }) => {
   return (
     <Portal>
-      <Dialog
-        visible={visible}
-      >
-        <Dialog.Title>{title}</Dialog.Title>
-        <Dialog.Content>
-          {DialogContent}
-        </Dialog.Content>
-        <Dialog.Actions>
-          {DialogActions}
-        </Dialog.Actions>
-      </Dialog>
+      <ScreenDarkModal>
+        <Dialog
+          style={styles.dialog}
+          visible={visible}
+          dismissable={false}
+        >
+          <Dialog.Title style={styles.dialogTitle}>{title}</Dialog.Title>
+          <Dialog.Content>{DialogContent}</Dialog.Content>
+          <Dialog.Actions>{DialogActions}</Dialog.Actions>
+        </Dialog>
+      </ScreenDarkModal>
     </Portal>
   );
 };
+
+const styles = StyleSheet.create({
+  dialog: { backgroundColor: darkColor },
+  dialogTitle: { color: whiteColor },
+});
 
 export default AlertDialog;
