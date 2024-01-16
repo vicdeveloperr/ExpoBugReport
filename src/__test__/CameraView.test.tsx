f2bac095804545ee6bcf9c32290f2f10f856d406
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-16 16:55:06 +0000

    Unifica responsabilidad y agrega test de CameraView

--- src/__test__/CameraView.test.tsx
+++ src/__test__/CameraView.test.tsx
@@ -1,44 +1,21 @@
-// Cámara se renderiza
-// Cámara frontal se renderiza
-// Se solicita los permisos al usuario, antes de renderizar la cámara
-
-import { render, screen, act, waitFor } from "@testing-library/react-native";
+import { render, screen } from "@testing-library/react-native";
 import CameraView from "../components/camerascreen/CameraView";
-import { Camera, CameraType } from "expo-camera";
+import { CameraType } from "expo-camera";
 import { View } from "react-native";
 
-describe("<Camera />", () => {
-  it("Camera se renderiza correctamente", () => {
-    jest.mock("expo-camera", () => ({
-      ...jest.requireActual("expo-camera"),
-      useCameraPermissions: () => ({
-        status: "granted",
-        request: jest.fn(),
-      }),
-      useMicrophonePermissions: () => ({
-        status: "granted",
-        request: jest.fn(),
-      }),
-    }));
-    render(
-      <CameraView cameraType={CameraType.front}>
+describe("<CameraView />", () => {
+  it("CameraView se renderiza correctamente", async () => {
+  render(
+      <CameraView
+        isCameraPermissionGranted={true}
+        isMicrophonePermissionGranted={true}
+        cameraType={CameraType.front}
+      >
         <View testID="camera"></View>
       </CameraView>
     );
 
-    void (async () => {
-      await waitFor(() => {
-        const camera = screen.getByTestId("camera");
-
-        expect(camera).toBeDefined();
-        expect(camera.prop.cameraType).toBe("front");
-      });
-    })();
+    const camera = screen.getByTestId("camera");
+    expect(camera).toBeDefined();
   });
-
-  //   it('calls onCameraReady with the camera instance', () => {
-  //     const onCameraReady = jest.fn();
-  //     render(<CameraView cameraType={CameraType.back} onCameraReady={onCameraReady} />);
-  //     expect(onCameraReady).toHaveBeenCalledTimes(1);
-  //   });
 });
--- src/components/camerascreen/CameraView.tsx
+++ src/components/camerascreen/CameraView.tsx
@@ -7,38 +7,32 @@ interface CameraViewProps {
   cameraType: CameraType;
   onCameraReady?: (camera: Camera) => void;
   children: React.ReactNode;
+  isCameraPermissionGranted: boolean;
+  isMicrophonePermissionGranted: boolean;
 }
 
 const CameraView: React.FC<CameraViewProps> = ({
   cameraType,
   onCameraReady,
   children,
+  isCameraPermissionGranted,
+  isMicrophonePermissionGranted,
 }) => {
   const camRef = useRef<Camera>(null);
-  const [statusCameraPermission, requestCameraPermission] =
-    Camera.useCameraPermissions();
-  const [statusMicrophonePermission, requestMicrophonePermission] =
-    Camera.useMicrophonePermissions();
+  // const [statusCameraPermission, requestCameraPermission] =
+  //   Camera.useCameraPermissions();
+  // const [statusMicrophonePermission, requestMicrophonePermission] =
+  //   Camera.useMicrophonePermissions();
 
-  useEffect(() => {
-    if (statusCameraPermission != null) {
-      void requestCameraPermission();
-    } else if (statusMicrophonePermission != null) {
-      void requestMicrophonePermission();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (statusCameraPermission != null) {
+  //     void requestCameraPermission();
+  //   } else if (statusMicrophonePermission != null) {
+  //     void requestMicrophonePermission();
+  //   }
+  // }, []);
 
-  if (statusCameraPermission != null && statusMicrophonePermission != null) {
-    return (
-      <Camera
-        ref={camRef}
-        style={styles.camera}
-        type={cameraType}
-      >
-        {children}
-      </Camera>
-    );
-  } else if (statusCameraPermission != null) {
+  if (isCameraPermissionGranted && isMicrophonePermissionGranted) {
     return (
       <Camera
         ref={camRef}
