192ce827a5eebdc437e91306664d74ffef32e6c4
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-17 13:39:15 +0000

    Crea interface cameraTypeStore para useCameraTypeStore

--- src/stateManagement/useCameraTypeStore.ts
+++ src/stateManagement/useCameraTypeStore.ts
@@ -2,7 +2,12 @@ import { create } from "zustand";
 import { CameraType as CameraOrientation } from "expo-camera";
 import type { CameraType } from "expo-camera";
 
-const useCameraTypeStore = create((set) => ({
+interface cameraTypeStore {
+  cameraType: CameraType;
+  setCameraType: (newCameraType: CameraType) => void;
+}
+
+const useCameraTypeStore = create<cameraTypeStore>((set) => ({
   cameraType: CameraOrientation.front,
   setCameraType: (newCameraType: CameraType) => {
     set({ cameraType: newCameraType });
