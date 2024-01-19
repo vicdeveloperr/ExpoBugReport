d7abc870656e918dca23ebd2a9def57a7221d209
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-19 16:47:57 +0000

    Testeo nuevo a CameraScreen y mockea useCountdownStore

--- src/__test__/screens/CameraScreen.test.tsx
+++ src/__test__/screens/CameraScreen.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react-native";
 import CameraScreen from "../../screens/CameraScreen";
+import { useCountdownStore } from "../../stateManagement/stores";
 
 jest.mock("expo-camera", () => ({
   Camera: {
@@ -13,19 +14,29 @@ jest.mock("expo-camera", () => ({
   },
 }));
 
-jest.mock("../stateManagement/stores", () => ({
-  useCountdownStore: jest.fn(),
+jest.mock("../../stateManagement/stores", () => ({
+  useCountdownStore: jest.fn().mockReturnValue({
+    countdown: 3, // Valor inicial
+    startCountdown: jest.fn(),
+    resetCountdown: jest.fn(),
+  }),
 }));
 
-test("Renderiza los componentes cuando los permisos están concedidos", () => {
-  render(<CameraScreen navigation={{ navigate: jest.fn() }} />);
+describe("<CameraScreen />", () => {
+  it("Renderiza los componentes cuando los permisos están concedidos", () => {
+    render(<CameraScreen navigation={{ navigate: jest.fn() }} />);
 
-  // Verifica la presencia de los componentes
-  const cameraView = screen.getByTestId("camera-view");
-  const cameraControls = screen.getByTestId("camera-controls");
-  const cameraCountdownModal = screen.queryByTestId("camera-countdown-modal");
+    // Verifica la presencia de los componentes
+    const cameraView = screen.getByTestId("camera-view");
+    const cameraControls = screen.getByTestId("camera-controls");
+    const cameraCountdownModal = screen.queryByTestId("camera-countdown-modal");
 
-  expect(cameraView).toBeTruthy();
-  expect(cameraControls).toBeTruthy();
-  expect(cameraCountdownModal).toBeNull(); // No debería renderizarse inicialmente
+    expect(cameraView).toBeTruthy();
+    expect(cameraControls).toBeTruthy();
+    expect(cameraCountdownModal).toBeNull(); // No debería renderizarse inicialmente
+  });
+  it("Al iniciar grabación, CameraScreen inicia el temporizador", () => {
+    const { startCountdown } = useCountdownStore();
+    expect(startCountdown).toHaveBeenCalled();
+  });
 });
