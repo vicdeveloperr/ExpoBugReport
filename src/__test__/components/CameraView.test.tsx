bca35a4de6889d81f63669d1b0fa682ac0b710b0
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-22 15:12:23 +0000

    Modifica test aplicados a CameraView
    Agrega testeo a la renderización de un cuadro de diálogo, sino se conceden los permisos correspondientes.

--- src/__test__/components/CameraView.test.tsx
+++ src/__test__/components/CameraView.test.tsx
@@ -1,42 +1,27 @@
 import { render, screen, waitFor } from "@testing-library/react-native";
 import CameraView from "../../components/camerascreen/CameraView";
 import { View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-
-jest.mock("@react-navigation/native", () => ({
-  useNavigation: () => ({
-    navigate: jest.fn(),
-  }),
-}));
 
 describe("<CameraView />", () => {
-  const { navigate } = useNavigation();
-  function renderCameraView(
-    isCameraPermissionGranted: boolean,
-    isMicrophonePermissionGranted: boolean
-  ): void {
+  beforeEach(() => {
     render(
-      <CameraView
-        isCameraPermissionGranted={isCameraPermissionGranted}
-        isMicrophonePermissionGranted={isMicrophonePermissionGranted}
-      >
+      <CameraView>
         <View testID="camera">Camera</View>
       </CameraView>
     );
-  }
+  });
 
   it("CameraView se renderiza correctamente", async () => {
-    renderCameraView(true, true);
     await waitFor(() => {
       const camera = screen.getByTestId("camera");
       expect(camera).toBeDefined();
     });
   });
 
-  it("Redirigir al usuario a la página principal, si no se conceden los permisos necesarios", async () => {
-    renderCameraView(false, false);
+  it("Si no se conceden los permisos necesarios, muestra cuadro de diálogo", async () => {
     await waitFor(() => {
-      expect(navigate).toHaveBeenCalled();
+      const alertDialog = screen.getByText("AlertDialog");
+      expect(alertDialog).toBeDefined();
     });
   });
 });
