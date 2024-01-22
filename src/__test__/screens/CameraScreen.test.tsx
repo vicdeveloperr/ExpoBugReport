d9fb612b5e097e6d22c074ba28b9e385bb44dd91
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-22 18:06:16 +0000

    Testea renderización de elementos de CameraScreen

--- src/__test__/screens/CameraScreen.test.tsx
+++ src/__test__/screens/CameraScreen.test.tsx
@@ -1,41 +1,39 @@
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
+import { render, screen, waitFor } from "@testing-library/react-native";
 import CameraScreen from "../../screens/CameraScreen";
 import { useCountdownStore } from "../../stateManagement/stores";
 
-jest.mock("../../components/camerascreen/CameraControls", () => "View");
-jest.mock("../../components/camerascreen/CameraCoundownModal", () => "View");
-jest.mock("../../components/camerascreen/CameraView", () => "View");
-
-jest.mock("expo-camera", () => ({
-  Camera: {
-    useCameraPermissions: () => ["granted"],
-    useMicrophonePermissions: () => ["granted"],
-  },
+jest.mock("@react-navigation/native", () => ({
+  useNavigation: () => ({ navigate: jest.fn() }),
 }));
 
 jest.mock("../../stateManagement/stores", () => ({
   useCountdownStore: jest.fn().mockReturnValue({
-    countdown: 3, // Valor inicial
+    countdown: 3,
     startCountdown: jest.fn(),
     resetCountdown: jest.fn(),
   }),
 }));
 
 describe("<CameraScreen />", () => {
-  it("Renderiza los componentes cuando los permisos están concedidos", () => {
-    render(<CameraScreen navigation={{ navigate: jest.fn() }} />);
-
-    // Verifica la presencia de los componentes
-    const cameraView = screen.getByTestId("camera-view");
-    const cameraControls = screen.getByTestId("camera-controls");
-    const cameraCountdownModal = screen.queryByTestId("camera-countdown-modal");
-
-    expect(cameraView).toBeTruthy();
-    expect(cameraControls).toBeTruthy();
-    expect(cameraCountdownModal).toBeNull(); // No debería renderizarse inicialmente
+  beforeEach(() => {
+    const navigation = { navigate: jest.fn(), addListener: jest.fn() };
+    render(<CameraScreen navigation={navigation} />);
   });
-  it("Al iniciar grabación, CameraScreen inicia el temporizador", () => {
+
+  it("Renderiza todos sus componentes cuando los permisos están concedidos", async () => {
+    await waitFor(() => {
+      const cameraView = screen.getByTestId("CameraView");
+      const cameraControls = screen.getByTestId("CameraControls");
+      const cameraCountdownModal = screen.queryByTestId("CameraCountdownModal");
+
+      expect(cameraView).toBeTruthy();
+      expect(cameraControls).toBeTruthy();
+      expect(cameraCountdownModal).toBeNull();
+    });
+  });
+
+  it("Al iniciar grabación, CameraScreen inicia el temporizador y muestra CameraCountdownModal", () => {
     const { startCountdown } = useCountdownStore();
     expect(startCountdown).toHaveBeenCalled();
   });
