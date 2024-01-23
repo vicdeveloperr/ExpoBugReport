fe9cab29f7b497f966a825f38bff3970b6a6a143
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-23 14:18:14 +0000

    Crea CancelAlertRecording
    Cuádro de diálogo para CameraScreen

---
+++ src/components/camerascreen/CancelAlertRecording.tsx
@@ -0,0 +1,10 @@
+import { TouchableOpacity, Text } from "react-native";
+import AlertDialog from "../AlertDialog";
+
+const CancelAlertRecording: React.FC = () => {
+  
+
+  return <AlertDialog />;
+};
+
+export default CancelAlertRecording;
