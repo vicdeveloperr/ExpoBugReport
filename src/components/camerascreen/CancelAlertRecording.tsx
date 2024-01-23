0f3871863b1e6d554945f886910dbbf83cb64ad8
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-23 15:36:39 +0000

    Utiliza AlertDialog en CancelAlertRecording

--- src/components/camerascreen/CancelAlertRecording.tsx
+++ src/components/camerascreen/CancelAlertRecording.tsx
@@ -1,10 +1,23 @@
-import { TouchableOpacity, Text } from "react-native";
 import AlertDialog from "../AlertDialog";
+import AlertDialogButton from "../AlertDialogButton";
+
+const ActionButtons: React.FC = () => {
+  return (
+    <>
+      <AlertDialogButton text="Continuar" />
+      <AlertDialogButton text="Detener" />
+    </>
+  );
+};
 
 const CancelAlertRecording: React.FC = () => {
-  
-
-  return <AlertDialog />;
+  return (
+    <AlertDialog
+      title="Grabación en curso"
+      description="Detenga la grabación, antes de abandonar."
+      DialogActions={<ActionButtons />}
+    />
+  );
 };
 
 export default CancelAlertRecording;
