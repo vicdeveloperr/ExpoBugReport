347a9ccbcd6c023c448e3e85aa66d38f59fc0b1b
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-01-24 14:07:17 +0000

    Modifica test de CameraScreen
    Testea renderización de children.

--- src/__test__/screens/CameraScreen.test.tsx
+++ src/__test__/screens/CameraScreen.test.tsx
@@ -1,54 +1,29 @@
-import React from "react";
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  act,
-} from "@testing-library/react-native";
+import { render, screen, waitFor } from "@testing-library/react-native";
 import CameraScreen from "../../screens/CameraScreen";
+import { View } from "react-native";
 
 jest.mock("@react-navigation/native", () => ({
-  useNavigation: () => ({ navigate: jest.fn() }),
-}));
-
-jest.mock("../../stateManagement/stores", () => ({
-  useCountdownStore: jest.fn().mockReturnValue({
-    countdown: 3,
-    startCountdown: jest.fn(),
-    resetCountdown: jest.fn(),
-  }),
+  useNavigation: () => ({ navigate: jest.fn(), addListener: jest.fn() }),
 }));
 
 describe("<CameraScreen />", () => {
-  beforeEach(() => {
-    const navigation = { navigate: jest.fn(), addListener: jest.fn() };
-    render(<CameraScreen navigation={navigation} />);
-  });
-
   it("Renderiza todos sus componentes cuando los permisos están concedidos", async () => {
+    render(<CameraScreen />);
     await waitFor(() => {
       const cameraView = screen.getByTestId("CameraView");
       const cameraControls = screen.getByTestId("CameraControls");
-      const cameraCountdownModal = screen.queryByTestId("CameraCountdownModal");
 
       expect(cameraView).toBeTruthy();
       expect(cameraControls).toBeTruthy();
-      expect(cameraCountdownModal).toBeNull();
     });
   });
 
-  it("Al iniciar grabación, CameraScreen inicia el temporizador y muestra CameraCountdownModal", async () => {
-    await waitFor(() => {
-      const stopRecordingButton = screen.getByTestId("recordButton");
+  it("Renderiza componente pasado por children", () => {
+    const Children: React.FC = () => <View testID="Children"></View>;
+    render(<CameraScreen children={<Children />} />);
 
-      act(() => {
-        fireEvent(stopRecordingButton, "press");
-      });
+    const childrenComponent = screen.getByTestId("Children");
 
-      const timer = screen.getByText("3");
-
-      expect(timer).toBeTruthy();
-    });
+    expect(childrenComponent).toBeTruthy();
   });
 });
