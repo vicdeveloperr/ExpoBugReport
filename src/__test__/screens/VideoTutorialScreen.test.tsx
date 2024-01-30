975a93bc646360f947d6639d1ac604aa2a844e59
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-01-30 16:33:06 +0000

    Testea children de VideoTutorialScreen
    Testea que pueda pasarse un React.Fragment como children, y que se renderice correctamente.

--- src/__test__/screens/VideoTutorialScreen.test.tsx
+++ src/__test__/screens/VideoTutorialScreen.test.tsx
@@ -2,15 +2,30 @@ import "@testing-library/react-native/extend-expect";
 import { View } from "react-native";
 import VideoTutorialScreen from "../../screens/VideoTutorialScreen";
 import { render, screen } from "@testing-library/react-native";
+import React from "react";
+
+function renderVideoTutorialScreenWith(children: React.ReactElement): void {
+  render(<VideoTutorialScreen>{children}</VideoTutorialScreen>);
+}
 
 describe("<VideoTutorialScreen />", () => {
-  beforeEach(() => {
+  it("Renderiza correctamente sus childrens", () => {
     const children = <View testID="children"></View>;
-    render(<VideoTutorialScreen>{children}</VideoTutorialScreen>);
+    renderVideoTutorialScreenWith(children);
+
+    const childrenInTheVirtualDOM = screen.getByTestId("children");
+    expect(childrenInTheVirtualDOM).toBeTruthy();
   });
 
-  it("Renderiza correctamente sus childrens", () => {
-    const children = screen.getByTestId("children");
-    expect(children).toBeTruthy();
+  it("Renderiza correctamente ReactFragment como children", () => {
+    const children = (
+      <>
+        <View testID="children"></View>
+      </>
+    );
+    renderVideoTutorialScreenWith(children);
+
+    const childrenInTheVirtualDOM = screen.getByTestId("children");
+    expect(childrenInTheVirtualDOM).toBeTruthy();
   });
 });
