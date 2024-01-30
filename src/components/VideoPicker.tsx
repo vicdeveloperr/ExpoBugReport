824cea2ee7c4a6720142428f161bb6ce3724faf9
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-01-30 13:51:03 +0000

    Expulsa pickVideo de VideoPicker
    Crea utilidad pickVideoFromGallery, que tiene la misma función que pickVideo.

--- src/components/VideoPicker.tsx
+++ src/components/VideoPicker.tsx
@@ -1,4 +1,3 @@
-import * as ImagePicker from "expo-image-picker";
 import FormattedIcon from "./FormattedIcon";
 import { TouchableOpacity, StyleSheet } from "react-native";
 
@@ -6,27 +5,6 @@ interface VideoPickerProps {
   onPressAction: () => Promise<void>;
 }
 const VideoPicker: React.FC<VideoPickerProps> = ({ onPressAction }) => {
-  const pickVideo = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
-        allowsEditing: true,
-        quality: 1,
-      });
-
-      if (!result.canceled) {
-        const video = result.assets[0];
-        console.log("Información del video seleccionado:", result);
-        if (video.duration && video.duration < 10000) {
-          navigation.navigate("loadVideo");
-        } else {
-          alert("El vídeo seleccionado debe durar menos de 10 segundos.");
-        }
-      }
-    } catch (error) {
-      console.error("Error al seleccionar el video:", error);
-    }
-  };
 
   return (
     <TouchableOpacity
---
+++ src/utils/pickVideoFromGallery.ts
@@ -0,0 +1,34 @@
+import * as ImagePicker from "expo-image-picker";
+import { useNavigation } from "@react-navigation/native";
+import type { VideoTutorialScreenProps } from "../screens/VideoTutorialScreen";
+import { useVideoPickerErrorDialogVisibilityStore } from "../stateManagement";
+
+const pickVideo: () => Promise<void> = async () => {
+  type navigationObject = VideoTutorialScreenProps["navigation"];
+  const { navigate } = useNavigation<navigationObject>();
+  const { setVideoPickerErrorDialogVisibility } =
+    useVideoPickerErrorDialogVisibilityStore();
+
+  try {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const video = result.assets[0];
+      console.log("Información del video seleccionado:", result);
+
+      if (video.duration != null && video.duration < 10000) {
+        navigate("loadVideo");
+      } else {
+        setVideoPickerErrorDialogVisibility(true);
+      }
+    }
+  } catch (error) {
+    console.error("Error al seleccionar el video:", error);
+  }
+};
+
+export default pickVideo();
