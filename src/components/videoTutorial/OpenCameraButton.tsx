66aaed768cae9d46be27aad0bd068cf000d4f182
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-01-30 17:03:47 +0000

    Crea videoTutorial/OpenCameraButton
    Este componente permite desresponsabilizar del botón de abrir cámara a VideoTutorialScreen.

---
+++ src/components/videoTutorial/OpenCameraButton.tsx
@@ -0,0 +1,31 @@
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import FormattedIcon from "../FormattedIcon";
+
+interface OpenCameraButtonProps {
+  onPressAction: () => void;
+}
+
+const OpenCameraButton: React.FC<OpenCameraButtonProps> = ({
+  onPressAction,
+}) => {
+  return (
+    <View style={styles.buttonOpenCameraContainer}>
+      <TouchableOpacity onPress={onPressAction}>
+        <FormattedIcon
+          size="big"
+          name="camera"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOpenCameraContainer: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
+export default OpenCameraButton;
