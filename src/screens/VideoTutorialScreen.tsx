5aa1569cc95fd46487f4307d0fac4632ff5a4733
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-01 14:40:35 +0000

    Soluciona errores en VideoTutorialScreen
    Elimina funciones y hooks innecesarios, y define styles.

--- src/screens/VideoTutorialScreen.tsx
+++ src/screens/VideoTutorialScreen.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import ScreenContainer from "../components/ScreenContainer";
-import { useBtnPlayModalStore, useVideoPlayerStore } from "../stateManagement/";
 import Loader from "../components/Loader";
 import type { StackNavigationProp } from "@react-navigation/stack";
 import type { RootStackParamList } from "../navigation/StackNavigator";
+import { StyleSheet } from "react-native";
+import { useVideoTutorialLoadingState } from "../stateManagement";
 
 interface VideoTutorialScreenProps {
   children: React.ReactElement;
@@ -17,54 +17,23 @@ export type VideoTutorialNavigationObject = StackNavigationProp<
 const VideoTutorialScreen: React.FC<VideoTutorialScreenProps> = ({
   children,
 }) => {
-  const { isBtnPlayVisible, toggleBtnPlay } = useBtnPlayModalStore(
-    (state) => state
-  );
-  const { setPlaying } = useVideoPlayerStore((state) => state);
-  const [isLoading, setIsLoading] = useState(true);
-
-  function openCamera(): void {
-    navigation.navigate("camera");
-  }
-
-  function playVideo(): void {
-    toggleBtnPlay();
-    setPlaying(true);
-  }
+  const { isLoading } = useVideoTutorialLoadingState((state) => state);
 
   return (
     <>
-      <ScreenContainer styles={styles.container}>
-        {children}
-        
-      </ScreenContainer>
+      <ScreenContainer styles={styles.container}>{children}</ScreenContainer>
       {isLoading && <Loader />}
     </>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     position: "relative",
-//     paddingTop: 0,
-//     display: "flex",
-//     justifyContent: "flex-end",
-//   },
-//   buttonsContainer: {
-//     width: "100%",
-//     display: "flex",
-//     justifyContent: "center",
-//     zIndex: 1,
-//   },
-//   buttonOpenCameraContainer: {
-//     width: "100%",
-//     display: "flex",
-//     alignItems: "center",
-//   },
-//   buttonPlayVideo: {
-//     width: "100%",
-//     height: "100%",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    paddingTop: 0,
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+});
 
 export default VideoTutorialScreen;
