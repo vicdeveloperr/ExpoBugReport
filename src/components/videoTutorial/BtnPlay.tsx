752b2c945d76434298022e0f669d6cc72c5a3eb5
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-01 14:45:54 +0000

    Condiciona renderizado de BtnPlay

--- src/components/videoTutorial/BtnPlay.tsx
+++ src/components/videoTutorial/BtnPlay.tsx
@@ -3,23 +3,28 @@ import { TouchableOpacity, StyleSheet } from "react-native";
 import FormattedIcon from "../FormattedIcon";
 import { centerViewContentStyle } from "../../utils/genericStyles";
 import type { GestureResponderEvent } from "react-native";
+import { useBtnPlayModalStore } from "../../stateManagement";
 
 interface BtnPlayProps {
   onPressAction: (event?: GestureResponderEvent) => void;
 }
 
 const BtnPlay: React.FC<BtnPlayProps> = ({ onPressAction }) => {
-  return (
-    <ScreenDarkModal>
-      <TouchableOpacity
-        style={[styles.buttonPlayVideo, centerViewContentStyle]}
-        onPress={onPressAction}
-        testID="BtnPlay"
-      >
-        <FormattedIcon name="play" />
-      </TouchableOpacity>
-    </ScreenDarkModal>
-  );
+  const { isBtnPlayVisible } = useBtnPlayModalStore((state) => state);
+  
+  if (isBtnPlayVisible) {
+    return (
+      <ScreenDarkModal>
+        <TouchableOpacity
+          style={[styles.buttonPlayVideo, centerViewContentStyle]}
+          onPress={onPressAction}
+          testID="BtnPlay"
+        >
+          <FormattedIcon name="play" />
+        </TouchableOpacity>
+      </ScreenDarkModal>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
