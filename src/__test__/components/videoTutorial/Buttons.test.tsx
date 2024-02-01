45cc09e843edce6764b0aef6d5e31d7ec79a2e5a
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-01 14:36:25 +0000

    Testea que sus hooks hayan sean defined en Buttons

--- src/__test__/components/videoTutorial/Buttons.test.tsx
+++ src/__test__/components/videoTutorial/Buttons.test.tsx
@@ -2,9 +2,20 @@ import renderer from "react-test-renderer";
 import Buttons from "../../../components/videoTutorial/Buttons";
 import { View } from "react-native";
 import { render } from "@testing-library/react-native";
+import { useVideoTutorialLoadingState } from "../../../stateManagement";
+import { useButtonsActions } from "../../../components/videoTutorial/hooks/useButtonsActions";
 
 jest.mock("@react-navigation/native", () => ({
   useNavigation: () => ({ navigate: jest.fn(), addListener: jest.fn() }),
+}));
+jest.mock("../../../stateManagement/useVideoTutorialLoadingState", () =>
+  jest.fn(() => ({ setIsLoading: jest.fn() }))
+);
+jest.mock("../../../components/videoTutorial/hooks/useButtonsActions", () => ({
+  useButtonsActions: jest.fn(() => ({
+    openCamera: jest.fn(),
+    playVideo: jest.fn(),
+  })),
 }));
 
 describe("<Buttons />", () => {
@@ -18,5 +29,16 @@ describe("<Buttons />", () => {
     const { getByTestId } = render(<Buttons>{children}</Buttons>);
 
     expect(getByTestId("children")).toBeTruthy();
+  });
+
+  it("Invoca useVideoTutorialLoadingState y define sus propiedades", () => {
+    render(<Buttons />);
+    expect(useVideoTutorialLoadingState).toHaveBeenCalled();
+  });
+
+  it("Define los valores de playVideo y openCamera", () => {
+    render(<Buttons />);
+
+    expect(useButtonsActions).toHaveBeenCalled();
   });
 });
