b3d3f81f9583c58433002c305110660303df6a18
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-01 14:27:29 +0000

    Testea render de children en Buttons.test

--- src/__test__/components/videoTutorial/Buttons.test.tsx
+++ src/__test__/components/videoTutorial/Buttons.test.tsx
@@ -1,5 +1,7 @@
 import renderer from "react-test-renderer";
 import Buttons from "../../../components/videoTutorial/Buttons";
+import { View } from "react-native";
+import { render } from "@testing-library/react-native";
 
 jest.mock("@react-navigation/native", () => ({
   useNavigation: () => ({ navigate: jest.fn(), addListener: jest.fn() }),
@@ -9,5 +11,12 @@ describe("<Buttons />", () => {
   it("Renderiza todo correctamente", () => {
     const tree = renderer.create(<Buttons />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("Renderiza childrens correctamente", () => {
+    const children = <View testID="children"></View>;
+    const { getByTestId } = render(<Buttons>{children}</Buttons>);
+
+    expect(getByTestId("children")).toBeTruthy();
   });
 });
