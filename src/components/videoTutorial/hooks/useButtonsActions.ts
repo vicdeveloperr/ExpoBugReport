a93606e4469b7d756e5f7aee1e78d27322672e76
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-01 17:02:26 +0000

    Devuelve pickVideo de useButtonsActions

--- src/components/videoTutorial/hooks/useButtonsActions.ts
+++ src/components/videoTutorial/hooks/useButtonsActions.ts
@@ -3,11 +3,13 @@ import {
   useBtnPlayModalStore,
   useVideoPlayerStore,
 } from "../../../stateManagement";
-import type { VideoTutorialNavigationObject } from "../../../screens/VideoTutorialScreen";
+import type { VideoTutorialNavigationObject } from "../VideoTutorial";
+import pickVideoFromGallery from "../../../utils/pickVideoFromGallery";
 
 type ButtonsActions = () => {
   openCamera: () => void;
   playVideo: () => void;
+  pickVideo: () => Promise<void>;
 };
 
 export const useButtonsActions: ButtonsActions = () => {
@@ -24,5 +26,15 @@ export const useButtonsActions: ButtonsActions = () => {
     setPlaying(true);
   }
 
-  return { playVideo, openCamera };
+  async function pickVideo(): Promise<void> {
+    await pickVideoFromGallery()
+      .then(() => {
+        navigate("loadVideo");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return { playVideo, openCamera, pickVideo };
 };
