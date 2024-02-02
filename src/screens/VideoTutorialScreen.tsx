309e494561eab026388cfafcc309575f9e28ce7a
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-02 14:23:44 +0000

    Elimina VideoTutorial y mueve todo en VideoTScreen

--- src/screens/VideoTutorialScreen.tsx
+++ src/screens/VideoTutorialScreen.tsx
@@ -1,21 +1,35 @@
 import Buttons from "../components/videoTutorial/Buttons";
-import VideoTutorial from "../components/videoTutorial/VideoTutorial";
 import VideoTutorialPlayer from "../components/VideoTutorialPlayer";
 import { useVideoTutorialLoadingState } from "../stateManagement";
+import ScreenContainer from "../components/ScreenContainer";
+import Loader from "../components/Loader";
+import { StyleSheet } from "react-native";
 
 const VideoTutorialScreen: React.FC = () => {
   const { setIsLoading } = useVideoTutorialLoadingState();
 
   return (
-    <VideoTutorial>
-      <VideoTutorialPlayer
-        onLoadComplete={() => {
-          setIsLoading(false);
-        }}
-      />
-      <Buttons />
-    </VideoTutorial>
+    <>
+      <ScreenContainer styles={styles.container}>
+        <VideoTutorialPlayer
+          onLoadComplete={() => {
+            setIsLoading(false);
+          }}
+        />
+        <Buttons />
+      </ScreenContainer>
+      <Loader />
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: "relative",
+    paddingTop: 0,
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+});
 
 export default VideoTutorialScreen;
