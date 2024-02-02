e0ce445b4af0866634477c41a6b2e6f83070356f
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-02 14:05:59 +0000

    Maneja los posibles errores en pickVideoFromGallery

--- src/utils/pickVideoFromGallery.ts
+++ src/utils/pickVideoFromGallery.ts
@@ -1,27 +1,25 @@
 import * as ImagePicker from "expo-image-picker";
 
-type video = ImagePicker.ImagePickerAsset;
+export type video = ImagePicker.ImagePickerAsset;
 
-const pickVideoFromGallery: () => Promise<video | unknown> = async () => {
-  try {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
-      allowsEditing: true,
-      quality: 1,
-    });
+const pickVideoFromGallery: () => Promise<video> = async () => {
+  const result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+    allowsEditing: true,
+    quality: 1,
+  });
 
-    if (!result.canceled) {
-      const video = result.assets[0];
-      console.log("Información del video seleccionado:", result);
+  if (!result.canceled) {
+    const video = result.assets[0];
+    console.log("Información del video seleccionado:", result);
 
-      if (video.duration != null && video.duration < 10000) {
-        return video;
-      } else {
-        throw new Error("El video seleccionado dura más de 10 segundos");
-      }
+    if (video.duration != null && video.duration < 10000) {
+      return video;
+    } else {
+      throw new Error("El video seleccionado dura más de 10 segundos");
     }
-  } catch (err) {
-    return err;
+  } else {
+    throw new Error("Ningún vídeo fue seleccionado");
   }
 };
 
