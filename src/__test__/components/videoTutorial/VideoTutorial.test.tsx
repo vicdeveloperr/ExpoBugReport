3c1ee27c41cba906d6e6f58aaeff941f62079d7f
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-02 17:28:51 +0000

    Crea test para VideoTutorialPlayer
    Nombra el test como VideoTutorial, porque este test solamente testeará al componente que tenga la responsabilidad de renderizar el vídeo tutorial.

---
+++ src/__test__/components/videoTutorial/VideoTutorial.test.tsx
@@ -0,0 +1,8 @@
+import { VideoTutorial } from "../../../components/videoTutorial/VideoTutorial";
+import renderer from "react-test-renderer";
+
+it("Renderiza el componente VideoTutorialPlayer con la estructura esperada", () => {
+  const tree = renderer.create(<VideoTutorial sourceUri="" />).toJSON;
+
+  expect(tree).toMatchSnapshot();
+});
