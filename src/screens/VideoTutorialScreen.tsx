b42cd2931152c5ee68fe582fe77b3ab9dfadccfa
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-16 14:57:17 +0000

    Usa onVideoLoadComplete en VideoTutorialScreen

--- src/screens/VideoTutorialScreen.tsx
+++ src/screens/VideoTutorialScreen.tsx
@@ -9,13 +9,13 @@ import { useVideoTutorialActions } from "../components/videoTutorial/hooks/useVi
 
 const VideoTutorialScreen: React.FC = () => {
   const { isLoading } = useVideoTutorialLoadingState();
-  const { togglePlay } = useVideoTutorialActions();
+  const { togglePlay, onVideoLoadComplete } = useVideoTutorialActions();
 
   return (
     <>
       <ScreenContainer styles={styles.container}>
         <VideoTutorialController onPressAction={togglePlay}>
-          <VideoTutorial sourceUri="https://player.vimeo.com/external/454804335.hd.mp4?s=3b45d2b6dcad98317fab4904f4645710a8f9a3cc&profile_id=174&oauth2_token_id=57447761" />
+          <VideoTutorial onLoadComplete={onVideoLoadComplete} sourceUri="https://player.vimeo.com/external/454804335.hd.mp4?s=3b45d2b6dcad98317fab4904f4645710a8f9a3cc&profile_id=174&oauth2_token_id=57447761" />
         </VideoTutorialController>
         <Buttons />
       </ScreenContainer>
