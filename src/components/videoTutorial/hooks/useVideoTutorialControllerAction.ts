85ee46602268d811a6b85f19a02d22a7e12fab54
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-02-16 14:50:55 +0000

    Devuelve función de useVideoTutorialControllerAction

--- src/components/videoTutorial/hooks/useVideoTutorialControllerAction.ts
+++ src/components/videoTutorial/hooks/useVideoTutorialControllerAction.ts
@@ -1,26 +1,36 @@
 import { videoRef } from "../VideoTutorial";
-import { useVideoPlayerStore, useBtnPlayModalStore } from "../../../stateManagement";
+import {
+  useVideoPlayerStore,
+  useBtnPlayModalStore,
+  useVideoTutorialLoadingState,
+} from "../../../stateManagement";
 
-type togglePlay = () => void;
+type action = () => void;
 type videoTutorialControllerAction = () => {
-    togglePlay: togglePlay;
-}
+  togglePlay: action;
+};
 
-export const useVideoTutorialControllerAction: videoTutorialControllerAction = () => {
+export const useVideoTutorialControllerAction: videoTutorialControllerAction =
+  () => {
     const { isPlaying, setPlaying } = useVideoPlayerStore((state) => state);
-  const { toggleBtnPlay } = useBtnPlayModalStore((state) => state);
+    const { toggleBtnPlay } = useBtnPlayModalStore((state) => state);
+    const { setIsLoading } = useVideoTutorialLoadingState((state) => state);
 
-  const togglePlay: togglePlay = () => {
-    if (videoRef.current != null) {
-      if (isPlaying) {
-        void videoRef.current.pauseAsync();
-      } else {
-        void videoRef.current.playAsync();
+    const togglePlay: action = () => {
+      if (videoRef.current != null) {
+        if (isPlaying) {
+          void videoRef.current.pauseAsync();
+        } else {
+          void videoRef.current.playAsync();
+        }
+        setPlaying(!isPlaying);
+        toggleBtnPlay();
       }
-      setPlaying(!isPlaying);
-      toggleBtnPlay();
-    }
-  };
+    };
 
-  return {togglePlay};
-}
+    const onVideoLoadComplete: action = () => {
+      setIsLoading(false);
+    };
+
+    return { togglePlay, onVideoLoadComplete };
+  };
