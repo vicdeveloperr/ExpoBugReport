a22c919afc6909e3a639a04f84192eb6e651e407
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-22 14:15:10 +0000

    Crea mock para extractVideoFrames.test

--- src/__test__/application/extractVideoFrames.test.tsx
+++ src/__test__/application/extractVideoFrames.test.tsx
@@ -1,6 +1,10 @@
+import { extractVideoFrames } from "../../application/extractVideoFrames";
+
+const framesExtractor = jest.fn(() => ["url", "url", "url", "url"]);
+
 describe("extractVideoFrames()", () => {
-  it("Al invocarlo con un vídeo como argumento, retorna un array con 4 URLs", () => {
-    const { images } = extractVideoFrames();
-    expect(images.lenght).toBe(4);
+  it("Al invocarlo con una función como argumento, retorna un array con 4 URLs", () => {
+    const images = extractVideoFrames(framesExtractor);
+    expect(images.length).toBe(4);
   });
 });
