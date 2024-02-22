7ee0e38ef539056d2c6a043dd7e74d090cd04095
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-22 14:07:19 +0000

    Modifica test de extractVideoFrames
    Elimina test de retorno de url

--- src/__test__/application/extractVideoFrames.test.tsx
+++ src/__test__/application/extractVideoFrames.test.tsx
@@ -1,10 +1,6 @@
 describe("extractVideoFrames()", () => {
-  it("Al invocarlo con un vídeo como argumento, retorna 4 frames del vídeo", () => {
+  it("Al invocarlo con un vídeo como argumento, retorna un array con 4 URLs", () => {
     const { images } = extractVideoFrames();
     expect(images.lenght).toBe(4);
-  });
-  it("Al invocarlo con un vídeo como argumento, retorna url en dónde se almacenan los frames extraídos", () => {
-    const { url } = extractVideoFrames();
-    expect(url).toBeTruthy();
   });
 });
