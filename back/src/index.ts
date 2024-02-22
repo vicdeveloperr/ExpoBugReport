d210744224df8d1b9fc375a306c48e12f8550931
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-22 17:23:14 +0000

    Usa endpoint extractVideoFrames

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import { extractFrames } from "./extractVideoFrames";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .use(extractFrames)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-); 
+);
