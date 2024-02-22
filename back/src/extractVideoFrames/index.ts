d5ea2d69a3aaa39d446d6d1de618930a02c3e7e5
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-22 17:05:50 +0000

    Crea carpet extractVideoFrames/ para esta funcionalidad

---
+++ back/src/extractVideoFrames/index.ts
@@ -0,0 +1,6 @@
+import { Elysia } from "elysia";
+
+export const extractFrames = new Elysia()
+    .post("/extractVideoFrames", () => {
+        
+    })
--- back/src/index.ts
+++ back/src/index.ts
@@ -4,4 +4,4 @@ const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+); 
