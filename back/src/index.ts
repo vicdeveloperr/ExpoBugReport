5e78827980ab74ea9ef0d3380618c7c3c607f00a
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-27 16:00:14 +0000

    Usa middleware para /analyzeVideo en index

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import analyzeVideo from "./routes/analyzeVideo/analyzeVideo";
 import { mainEnpointUrl } from "./utils/mainEnpointUrl";
+import analyzeVideoValidatorMiddlewares from "./middlewares/analyzeVideo/analyzeVideoValidatorMiddlewares";
 
 const app = new Hono();
 
 app.route(`${mainEnpointUrl}:movement`, analyzeVideo);
+app.route(`${mainEnpointUrl}:movement`, analyzeVideoValidatorMiddlewares);
 
 export default app;
