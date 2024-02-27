270bfba56198e555c6d214ff43e419c6a314259f
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-27 15:30:44 +0000

    Testea que se envíe parámetros a POST /analyzeVideo

--- back/src/routes/analyzeVideo/analyzeVideo.test.ts
+++ back/src/routes/analyzeVideo/analyzeVideo.test.ts
@@ -6,45 +6,33 @@ const apiUrl = getApiUrl();
 
 mock.module("../../analyzeVideo/application/useAnalyzeVideo.ts", () => mock());
 
+function request(body: FormData) {
+  const req = new Request(`${apiUrl}/analyzeVideo/allen iverson cross`, {
+    body: body,
+    method: "POST",
+  });
+
+  return req;
+}
+
 describe("POST /analyzeVideo", () => {
   it("Retorna status 200 y análisis generado por la IA, si el cuerpo de la solicitud es correcto", async () => {
     const videoToAnalyze = new FormData();
-    videoToAnalyze.append("video", {
+    videoToAnalyze.append("file", {
       uri: "",
       name: "video.mp4",
       type: "video/mp4",
     } as any);
-    const requestBody = {
-      video: videoToAnalyze,
-      movement: "allen iverson cross",
-    };
-    const req = new Request(`${apiUrl}/analyzeVideo`, {
-      body: JSON.stringify(requestBody),
-      method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+    // const requestBody = {
+    //   video: videoToAnalyze,
+    //   movement: "allen iverson cross",
+    // };
+    const req = request(videoToAnalyze);
 
     const res = await app.request(req);
     expect(res.status).toBe(200);
     expect(await res.json()).toEqual({
       message: "",
     });
-  });
-
-  it("Retorna 400 y un mensaje de que el video no ha podido ser procesado, si el cuerpo de la solicitud no es el esperado", async () => {
-    const req = new Request(`${apiUrl}/analyzeVideo`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-
-    const errorMsg =
-      "El vídeo no ha podido ser analizado. Por favor, vuelva a intentarlo";
-    const res = await app.request(req);
-    expect(await res.text()).toBe(errorMsg);
-    expect(res.status).toBe(200);
   });
 });
