ba9f7d37c9bf0e0f1bbe3d6873044ebd79a91adc
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-28 16:15:12 +0000

    Modifica implementación de modelConsultor
    modelConsultor utiliza funciones de guardar vídeo, extraer sus frames, y mandar procesarlos.

--- back/src/analyzeVideo/infractructure/modelConsultor.ts
+++ back/src/analyzeVideo/infractructure/modelConsultor.ts
@@ -1,12 +1,17 @@
 import ollama from "ollama";
 import { tModelConsultor } from "../domain/types";
 import { framesExtractor } from "./framesExtractor";
+import path from "path";
+import { dataStoragePath } from "../../utils/dataStoragePath";
+import { saveVideo } from "./saveVideo";
 
 export const modelConsultor: tModelConsultor = async (
   video,
   movementToImprove
 ) => {
-  const frames = await framesExtractor(videoUri);
+  const videoPath = path.join(dataStoragePath, video.name);
+  await saveVideo(video);
+  const frames = await framesExtractor(videoPath);
   const imagesToProcess = frames ? frames : undefined;
 
   const response = await ollama.chat({
--- back/src/analyzeVideo/infractructure/saveVideo.ts
+++ back/src/analyzeVideo/infractructure/saveVideo.ts
@@ -1,9 +1,10 @@
 import { writeFile } from "fs/promises";
+import { dataStoragePath } from "../../utils/dataStoragePath";
 
-export const saveVideo = async (video: File, path: string) => {
+export const saveVideo = async (video: File) => {
   try {
     const buffer = await video.arrayBuffer();
-    await writeFile(path, Buffer.from(buffer));
+    await writeFile(dataStoragePath, Buffer.from(buffer));
   } catch (err) {
     throw new Error();
   }
