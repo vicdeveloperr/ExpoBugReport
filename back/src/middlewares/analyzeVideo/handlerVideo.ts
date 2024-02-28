f7bf229647f068d2ff376f1938e2fbf65906b346
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-28 12:59:19 +0000

    Elimina módulos innecesarios

--- back/src/middlewares/analyzeVideo/handlerVideo.ts
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const upload = multer({ dest: "/workspaces/ChossApp/back/assets" });
-
-export const handlerVideo = (name: string) => {
-  const result = upload.single(name);
-
-  return result;
-};
