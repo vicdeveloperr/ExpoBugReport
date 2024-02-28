a465ad0975935c2fcf45ce18b467b854671ebc28
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-28 12:57:32 +0000

    Deshace import inútil en index.ts

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import analyzeVideo from "./routes/analyzeVideo/analyzeVideo";
 import { mainEnpointUrl } from "./utils/mainEnpointUrl";
-import analyzeVideoValidatorMiddlewares from "./middlewares/analyzeVideo/analyzeVideoValidatorMiddlewares";
 
 const app = new Hono();
 
