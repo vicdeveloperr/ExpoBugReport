b5e292235ea24e323753cdfb3bb91c5d9a751f77
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-29 14:08:16 +0000

    Usa Bun.write en saveVideo

--- back/src/analyzeVideo/infractructure/saveVideo.ts
+++ back/src/analyzeVideo/infractructure/saveVideo.ts
@@ -1,11 +1,10 @@
-import { writeFile } from "fs/promises";
 import path from "path";
 
 export const saveVideo = async (video: File) => {
   const videoPath = path.resolve("../../../data/video.mp4");
   try {
     const buffer = await video.arrayBuffer();
-    await writeFile(videoPath, Buffer.from(buffer));
+    await Bun.write(videoPath, Buffer.from(buffer));
   } catch (err) {
     console.log(err);
   }
