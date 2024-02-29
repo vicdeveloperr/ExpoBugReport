7aefa21ac458065dbf65995e10c7451da2e8c0fb
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-29 14:19:53 +0000

    Usa video.name en write en saveVideo

--- back/src/analyzeVideo/infractructure/saveVideo.ts
+++ back/src/analyzeVideo/infractructure/saveVideo.ts
@@ -1,9 +1,9 @@
 import path from "path";
 
 export const saveVideo = async (video: File) => {
-  const videoPath = path.resolve("../../../data", video.name);
+  const videoPath = path.resolve("../../../data");
   try {
-    await Bun.write(videoPath, video);
+    await Bun.write(video.name, video);
   } catch (err) {
     console.log(err);
   }
