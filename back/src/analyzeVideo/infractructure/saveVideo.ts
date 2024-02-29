8a717e4e7bd65c4e885068cdce75b0d9d8d18ea6
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-29 13:48:20 +0000

    Corrije storageData en saveVideo

--- back/src/analyzeVideo/infractructure/saveVideo.ts
+++ back/src/analyzeVideo/infractructure/saveVideo.ts
@@ -2,7 +2,7 @@ import { writeFile } from "fs/promises";
 import path from "path";
 
 export const saveVideo = async (video: File) => {
-  const storageData = path.resolve("../../../data", video.name);
+  const storageData = path.resolve("../../../data");
   try {
     const buffer = await video.arrayBuffer();
     await writeFile(storageData, Buffer.from(buffer));
