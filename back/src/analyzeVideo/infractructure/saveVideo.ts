3286493594aa08562b0b564d4616a8775969b130
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-02-29 14:23:11 +0000

    Guarda vídeo en data en saveVideo

--- back/src/analyzeVideo/infractructure/saveVideo.ts
+++ back/src/analyzeVideo/infractructure/saveVideo.ts
@@ -3,7 +3,7 @@ import path from "path";
 export const saveVideo = async (video: File) => {
   const videoPath = path.resolve("../../../data");
   try {
-    await Bun.write(video.name, video);
+    await Bun.write(`../../../data/${video.name}`, video);
   } catch (err) {
     console.log(err);
   }
