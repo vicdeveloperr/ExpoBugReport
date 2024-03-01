fd2b1afe3a0ea0690152b5a4a7ec679c8a918ed0
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-01 13:20:26 +0000

    Elimina capa de aplicación

--- front/src/application/extractVideoFrames.ts
+++
@@ -1,6 +0,0 @@
-type typeExtractVideoFrames = (framesExtractor: () => string[]) => string[] 
-
-export const extractVideoFrames: typeExtractVideoFrames = (framesExtractor) => {
-    const result = framesExtractor();
-    return result;
-}
