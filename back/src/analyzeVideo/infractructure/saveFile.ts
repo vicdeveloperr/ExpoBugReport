cd1563cbb8afa08ae484c91c901c9c622aec9d79
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-19 16:34:25 +0000

    Modifica path usado en saveFile

--- back/src/analyzeVideo/infractructure/saveFile.ts
+++ back/src/analyzeVideo/infractructure/saveFile.ts
@@ -1,11 +1,16 @@
+import { join as pathJoin } from "path";
+
 type tSaveFile = (
   file: string | Blob | NodeJS.TypedArray | ArrayBufferLike | Bun.BlobPart[],
   path: string
 ) => Promise<void>;
 
+import { storageDataPath } from "./storageDataPath";
+
 export const saveFile: tSaveFile = async (file, path) => {
   try {
-    await Bun.write(path, file);
+    const filePath = pathJoin(storageDataPath, path);
+    await Bun.write(filePath, file);
   } catch (err) {
     console.log(err);
   }
