4c0699c08d197885cd28ac3fac8bd86eae04b841
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-19 16:29:55 +0000

    Crea storageDataPath

--- back/src/analyzeVideo/infractructure/saveFile.ts
+++ back/src/analyzeVideo/infractructure/saveFile.ts
@@ -1,4 +1,9 @@
-export const saveFile = async (file: File, path: string) => {
+type tSaveFile = (
+  file: string | Blob | NodeJS.TypedArray | ArrayBufferLike | Bun.BlobPart[],
+  path: string
+) => Promise<void>;
+
+export const saveFile: tSaveFile = async (file, path) => {
   try {
     await Bun.write(path, file);
   } catch (err) {
---
+++ back/src/analyzeVideo/infractructure/storageDataPath.ts
@@ -0,0 +1,3 @@
+import path from "path";
+
+export const storageDataPath = path.resolve(__dirname, "../../../data");
