d7781ebdc42b876e6aab7afe99c7ecb3b4178b27
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-20 19:14:20 +0000

    Crea SpeechAnimation.tsx
    Este componente renderizará la animación de voz en la página de 'analysis'.

--- front/App.tsx
+++ front/App.tsx
@@ -2,12 +2,13 @@ import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
+import { SpeechAnimation } from "./src/components/analysis/SpeechAnimation";
 
 const App: React.FC = () => {
   return (
     <PaperProvider>
       <StatusBar style="auto" />
-      <StackNavigator />
+      <SpeechAnimation />
     </PaperProvider>
   );
 };
---
+++ front/src/components/analysis/SpeechAnimation.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export const SpeechAnimation: React.FC = () => {
+  return <View></View>;
+};
