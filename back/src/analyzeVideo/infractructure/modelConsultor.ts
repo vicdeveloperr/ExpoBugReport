6257cf7650e48b65be58bb0f7739cb9e283eb67d
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-20 18:39:30 +0000

    Usa uniqid para path de vídeo en modelConsultor

--- back/src/analyzeVideo/infractructure/modelConsultor.ts
+++ back/src/analyzeVideo/infractructure/modelConsultor.ts
@@ -4,12 +4,14 @@ import { framesExtractor } from "./framesExtractor";
 import path from "path";
 import { saveFile } from "./saveFile";
 import { storageDataPath } from "./storageDataPath";
+import uniqid from "uniqid";
 
 export const modelConsultor: tModelConsultor = async (
   video,
   movementToImprove
 ) => {
-  const videoPath = path.join(storageDataPath, video.name);
+  const filename = uniqid("", `-${video.name}`);
+  const videoPath = path.join(storageDataPath, filename);
   await saveFile(video, videoPath);
   const frames = await framesExtractor(videoPath);
   const imagesToProcess = frames ? frames : undefined;
