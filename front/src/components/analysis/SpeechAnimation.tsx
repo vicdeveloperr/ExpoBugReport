56856e1a992b55eda11c37f0b681de532609534b
Victor Ayala Santacruz <105018884+vicdeveloperr@users.noreply.github.com>
2024-03-21 17:32:39 +0000

    Estiliza SpeechAnimation

--- front/src/components/analysis/SpeechAnimation.tsx
+++ front/src/components/analysis/SpeechAnimation.tsx
@@ -1,5 +1,23 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Shadow } from "react-native-shadow-2";
 
 export const SpeechAnimation: React.FC = () => {
-  return <View></View>;
+  return (
+    <Shadow
+      startColor="#000"
+      style={styles.content}
+      distance={15}
+    >
+    </Shadow>
+  );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    borderRadius: 100,
+    width: 110,
+    height: 110,
+    borderWidth: 10,
+    borderColor: "#000",
+  },
+});
