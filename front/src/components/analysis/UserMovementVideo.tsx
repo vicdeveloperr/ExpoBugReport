57565619ceaa4dc3eaf326f2b3d2797896020e7f
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-22 19:17:24 +0000

    Crea esqueleto de UserMovementVideo (no funcional)

--- front/src/components/analysis/UserMovementVideo.tsx
+++ front/src/components/analysis/UserMovementVideo.tsx
@@ -1,5 +1,17 @@
+import Loader from "../Loader";
+import ScreenDarkModal from "../ScreenDarkModal";
+import { VideoFullScreen } from "../videoTutorial/VideoFullScreen";
+import { useState } from "react";
+
 export const UserMovementVideo = () => {
-    return (
-        
-    );
-}
+  const [isLoading, setLoading] = useState(true);
+
+  return (
+    <>
+      <ScreenDarkModal>
+        <VideoFullScreen />
+      </ScreenDarkModal>
+      <Loader isLoading={isLoading} />
+    </>
+  );
+};
