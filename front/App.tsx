d48e363965327f91a66c29c72a4f53e820f084d6
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-22 18:30:16 +0000

    Restaura App.tsx a su forma normal

--- front/App.tsx
+++ front/App.tsx
@@ -2,13 +2,12 @@ import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
-import { SpeechAnimation } from "./src/components/analysis/SpeechAnimation";
 
 const App: React.FC = () => {
   return (
     <PaperProvider>
       <StatusBar style="auto" />
-      <SpeechAnimation />
+      <StackNavigator />
     </PaperProvider>
   );
 };
