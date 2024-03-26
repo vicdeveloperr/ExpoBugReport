4abb00b2a8a2156931900ef674c94d1dc978715c
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-26 14:03:18 +0000

    Usa useStatusBarHeightStore en Nav de analysis

--- front/src/components/analysis/Nav.tsx
+++ front/src/components/analysis/Nav.tsx
@@ -3,16 +3,24 @@ import { small } from "../../utils/iconSizes";
 import { whiteColor } from "../../utils/colors";
 import { StyleSheet, View } from "react-native";
 import { SpeechAnimation } from "./SpeechAnimation";
+import { useStatusBarHeightStore } from "../../stateManagement";
 
 type NavType = () => React.ReactNode;
 
 export const Nav: NavType = () => {
+  const { height } = useStatusBarHeightStore((state) => state);
+
   return (
     <View
       testID="nav"
       style={styles.bg}
     >
-      <View style={styles.contentContainer}>
+      <View
+        style={[
+          styles.contentContainer,
+          { paddingBottom: height ? height : 0 },
+        ]}
+      >
         <AntDesign
           name="home"
           size={small}
