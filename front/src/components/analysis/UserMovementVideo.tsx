8f5eb414424b1088a40c180303b493f14c163eb1
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-26 16:00:51 +0000

    Agrega vídeo de placeholder en UserMovementVideo

--- front/src/components/analysis/UserMovementVideo.tsx
+++ front/src/components/analysis/UserMovementVideo.tsx
@@ -12,7 +12,9 @@ export const UserMovementVideo = () => {
     <>
       <ScreenDarkModal>
         <VideoFullScreen
-          sourceUri={recorded}
+          sourceUri={
+            "https://player.vimeo.com/progressive_redirect/playback/562969203/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=d823a8401b6f5f93bd4e73a4483fd2502327fc034d6f469ba6eb5da972b81ae0"
+          }
           onLoadComplete={() => setIsLoading(false)}
         />
       </ScreenDarkModal>
