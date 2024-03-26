e86d3fe2d97601a93188155c7f8a62fe09e265b5
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-26 13:26:41 +0000

    Usa useSetStatusBarHeight en el App.ts

--- front/App.tsx
+++ front/App.tsx
@@ -2,8 +2,11 @@ import "react-native-gesture-handler";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
+import { useSetStatusBarHeight } from "./src/utils/useSetStatusBarHeight";
 
 const App: React.FC = () => {
+  useSetStatusBarHeight(); // Almacenamos valor de la altura de la barra de estado
+
   return (
     <PaperProvider>
       <StatusBar style="auto" />
