36271fc58673945bbfe60dd25a471d78444e81ea
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-27 14:33:43 +0000

    Crea useOnStartRecording desde useRecordingEffects

---
+++ front/src/components/camerascreen/hooks/useOnStartRecording.ts
@@ -0,0 +1,35 @@
+import recordVideo from "../../../utils/recordVideo";
+import getVideoAnalysis from "../../../utils/getVideoAnalysis";
+import { useNavigation } from "@react-navigation/native";
+import { useRecordedStore } from "../../../stateManagement";
+import useHandlerStates from "./useHandlerStates";
+import { camRef } from "../CameraView";
+import { CameraScreenNavigator } from "../../../screens/CameraScreen";
+
+type onStartRecordingType = (useBeforeStart: () => void) => Promise<void>;
+
+export const useOnStartRecording: onStartRecordingType = async (
+  useBeforeStart
+) => {
+  useBeforeStart();
+
+  const { navigate } = useNavigation<CameraScreenNavigator>();
+  const { setRecorded } = useRecordedStore((state) => state);
+  const { resetCountdown, setIsRecording } = useHandlerStates();
+
+  if (camRef.current != null) {
+    await recordVideo(camRef.current)
+      .then(async (data) => {
+        if (typeof data === "string") {
+          setRecorded(data);
+          await getVideoAnalysis(data, "allen iverson cross");
+          navigate("analysis");
+        }
+        resetCountdown();
+        setIsRecording(false);
+      })
+      .catch((err: string) => {
+        console.log(err);
+      });
+  }
+};
--- front/src/components/camerascreen/hooks/useRecordingEffects.ts
+++ front/src/components/camerascreen/hooks/useRecordingEffects.ts
@@ -1,13 +1,9 @@
 import useHandlerStates from "./useHandlerStates";
-import recordVideo from "../../../utils/recordVideo";
 import { camRef } from "../CameraView";
 import stopVideoRecording from "../../../utils/stopVideoRecording";
 import { useNavigation } from "@react-navigation/native";
 import type { CameraScreenNavigator } from "../../../screens/CameraScreen";
 import { useRecordedStore } from "../../../stateManagement";
-import getVideoAnalysis from "../../../utils/getVideoAnalysis";
-
-type onStartRecordingType = (beforeStart: () => void) => Promise<void>;
 
 type typeUseRecordingEffects = () => {
   onStartRecording: onStartRecordingType;
@@ -15,37 +11,7 @@ type typeUseRecordingEffects = () => {
 };
 
 const useRecordingEffects: typeUseRecordingEffects = () => {
-  const { setIsTimerVisible, startCountdown, setIsRecording, resetCountdown } =
-    useHandlerStates();
-  const { navigate } = useNavigation<CameraScreenNavigator>();
-  const { setRecorded } = useRecordedStore((state) => state);
-
-  const onStartRecording: onStartRecordingType = async (beforeStart) => {
-    beforeStart();
-    // setIsTimerVisible(true);
-    // startCountdown(onFinishCountdown);
-
-    // function onFinishCountdown(): void {
-    //   setIsTimerVisible(false);
-    //   setIsRecording(true);
-    // }
-
-    if (camRef.current != null) {
-      await recordVideo(camRef.current)
-        .then(async (data) => {
-          if (typeof data === "string") {
-            setRecorded(data);
-            await getVideoAnalysis(data, "allen iverson cross");
-            navigate("analysis");
-          }
-          resetCountdown();
-          setIsRecording(false);
-        })
-        .catch((err: string) => {
-          console.log(err);
-        });
-    }
-  };
+  const { setIsRecording, resetCountdown } = useHandlerStates();
 
   const onStopRecording: () => void = () => {
     stopVideoRecording(camRef);
