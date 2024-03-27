fbd00da4af7ee449dcd11b4b8e7324972a2ea24f
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-27 19:18:03 +0000

    Arregla devoluciones de mock - useAnalyzeVideo.test

--- back/src/analyzeVideo/application/useAnalyzeVideo.test.ts
+++ back/src/analyzeVideo/application/useAnalyzeVideo.test.ts
@@ -1,12 +1,10 @@
-import { resolve } from "bun";
 import { useAnalyzeVideo } from "./useAnalyzeVideo";
 import { describe, it, mock, expect } from "bun:test";
-import { ChatResponse } from "ollama";
 import { AnalyzeVideoResult } from "../domain/types";
 
 const promise: Promise<AnalyzeVideoResult> = new Promise((resolve) => {
   resolve({
-    audio: "audio.mp3",
+    audioUrl: "audio.mp3",
   });
 });
 const dependencie = mock((videoUrl, movement) => promise);
@@ -24,7 +22,7 @@ describe("useAnalyzeVideo()", () => {
   it("Retorna análisis del vídeo en forma de audio", async () => {
     const result = await callUseAnalyzeVideo();
     expect(result).toEqual({
-      audio: "audio.mp3",
+      audioUrl: "audio.mp3",
     });
   });
 
