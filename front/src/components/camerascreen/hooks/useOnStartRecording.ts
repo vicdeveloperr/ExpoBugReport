777389cdb9e37bc1751b0b084a36467f70f55653
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-27 18:14:52 +0000

    Retorna función en useOnStartRecording

--- front/src/components/camerascreen/hooks/useOnStartRecording.ts
+++ front/src/components/camerascreen/hooks/useOnStartRecording.ts
@@ -6,36 +6,51 @@ import useHandlerStates from "./useHandlerStates";
 import { camRef } from "../CameraView";
 import { CameraScreenNavigator } from "../../../screens/CameraScreen";
 
-type onStartRecordingType = (
-  useBeforeStart: () => void,
-  afterEnd?: () => void
-) => Promise<void>;
+type onStartRecordingType = () => Promise<() => void>;
 
-export const useOnStartRecording: onStartRecordingType = async (
-  useBeforeStart,
-  afterEnd
-) => {
-  useBeforeStart();
+function useBeforeStart() {
+  const { setIsTimerVisible, startCountdown, setIsRecording } =
+    useHandlerStates();
+
+  const beforeStart = () => {
+    setIsTimerVisible(true);
+    startCountdown(onFinishCountdown);
+
+    function onFinishCountdown(): void {
+      setIsTimerVisible(false);
+      setIsRecording(true);
+    }
+  };
+
+  return beforeStart;
+}
+
+export const useOnStartRecording: onStartRecordingType = async () => {
+  const beforeStart = useBeforeStart();
 
   const { navigate } = useNavigation<CameraScreenNavigator>();
   const { setRecorded } = useRecordedStore((state) => state);
   const { resetCountdown, setIsRecording } = useHandlerStates();
 
-  if (camRef.current != null) {
-    await recordVideo(camRef.current)
-      .then(async (data) => {
-        if (typeof data === "string") {
-          setRecorded(data);
-          await getVideoAnalysis(data, "allen iverson cross");
-          navigate("analysis");
-        }
-        resetCountdown();
-        setIsRecording(false);
-      })
-      .catch((err: string) => {
-        console.log(err);
-      });
-  }
+  const onStartRecording = async () => {
+    beforeStart();
+    
+    if (camRef.current != null) {
+      await recordVideo(camRef.current)
+        .then(async (data) => {
+          if (typeof data === "string") {
+            setRecorded(data);
+            await getVideoAnalysis(data, "allen iverson cross");
+            navigate("analysis");
+          }
+          resetCountdown();
+          setIsRecording(false);
+        })
+        .catch((err: string) => {
+          console.log(err);
+        });
+    }
+  };
 
-  afterEnd ? afterEnd() : undefined;
+  return onStartRecording;
 };
