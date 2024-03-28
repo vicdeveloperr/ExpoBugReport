39b0e1209893d321648c1f8573a8560ab75e13d1
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-28 12:38:45 +0000

    Crea AnalysisScreenNavigator en types/

---
+++ front/src/types/AnalysisScreenNavigator.ts
@@ -0,0 +1,7 @@
+import type { RootStackParamList } from "../navigation/StackNavigator";
+import type { StackNavigationProp } from "@react-navigation/stack";
+
+export type AnalysisScreenNavigator = StackNavigationProp<
+  RootStackParamList,
+  "camera"
+>;
