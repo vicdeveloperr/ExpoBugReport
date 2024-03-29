c966631dd8367c66a9b1315a94c3648f1bdc6828
vicdeveloperrr <155172689+vicdeveloperrr@users.noreply.github.com>
2024-03-29 18:16:38 +0000

    Sirve archivos estáticos en /speechs

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
 import analyzeVideo from "./routes/analyzeVideo/analyzeVideo";
 import { mainEnpointUrl } from "./utils/mainEnpointUrl";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
+
+app.get(
+  "/speechs/*",
+  serveStatic({
+    root: "./",
+  })
+);
 
 app.route(`${mainEnpointUrl}:movement`, analyzeVideo);
 
